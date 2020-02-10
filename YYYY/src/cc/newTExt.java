71cda1162dca8277db9b7e3944300fb97dc4a933
86191 <86191@LAPTOP-50F1GPVF>
2020-02-10 09:04:27 +0800

    1

--- YYYY/src/cc/newTExt.java
+++ YYYY/src/cc/newTExt.java
@@ -1,5 +1,14 @@
 package cc;
 
 public class newTExt {
-
-}
+	String str = new String("good");
+	char[] ch = { 'a', 'b', 'c' };
+	public static void main(String args[]) {
+		newTExt ex = new newTExt();
+		ex.change(ex.str, ex.ch);
+		System.out.print(ex.str + " and ");
+		System.out.print(ex.ch); }
+	public static void change(String str, char ch[]) {
+		str = "test ok";
+		ch[0] = 'g';
+	}}
